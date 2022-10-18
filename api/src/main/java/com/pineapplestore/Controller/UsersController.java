48c1bb495fdc84843dd930bc8f636232dcd5e9c7
Linson <854700937@qq.com>
2022-10-19 00:20:19 +0800

    实现用户登录和用户注册的接口

---
+++ api/src/main/java/com/pineapplestore/Controller/UsersController.java
@@ -0,0 +1,44 @@
+package com.pineapplestore.Controller;
+
+
+import com.PineappleStore.ResultVo.ResultVo;
+import com.PineappleStore.service.UsersService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 用户  前端控制器
+ * </p>
+ *
+ * @author Linson
+ * @since 2022-10-17
+ */
+
+@Api(value = "用户", tags = "用户接口")
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UsersService usersService;
+
+    @GetMapping("/login")
+
+    public ResultVo login(@RequestParam String userName, @RequestParam(value = "passWrod") String passWrod) {
+        ResultVo resultVo = usersService.Login(userName, passWrod);
+        return resultVo;
+
+    }
+
+    @PostMapping("/resgit")
+    public ResultVo Resgit(@RequestParam String userName, @RequestParam(value = "passWrod") String passWrod) {
+        ResultVo resultVo = usersService.Resgit(userName, passWrod);
+        return resultVo;
+
+    }
+
+
+}
+

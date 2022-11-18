5bc324ab1071fd0711229ff5832c2f3080b1090f
Linson <854700937@qq.com>
2022-11-19 02:45:05 +0800

    添加短信发送，完善登录接口

--- api/src/main/java/com/PineappleStore/web/Controller/UsersController.java
+++ api/src/main/java/com/PineappleStore/web/Controller/UsersController.java
@@ -1,9 +1,12 @@
 package com.PineappleStore.web.Controller;
 
 
+import com.PineappleStore.ResultVo.ResultVo;
+import com.PineappleStore.ResultVo.StatusVo;
 import com.PineappleStore.ResultVo.TokenVo;
 import com.PineappleStore.service.UsersService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,20 +22,55 @@ import org.springframework.web.bind.annotation.*;
 @Api(value = "用户", tags = "用户接口")
 @RestController
 @RequestMapping("/users")
+
 public class UsersController {
 
     @Autowired
     private UsersService usersService;
 
+
+    @ApiOperation(value = "登录接口", notes = "登录接口")
     @PostMapping("/login")
-    @ResponseBody
-    public TokenVo login(@RequestParam String userName, @RequestParam String passWrod, @RequestParam String loginToken) throws Exception {
-        return usersService.Login(userName, passWrod, loginToken);
+    public TokenVo login(@RequestParam("userName") String userName, @RequestParam("passWord") String passWord, @RequestParam("loginToken") String loginToken) throws Exception {
+        return usersService.Login(userName, passWord, loginToken);
     }
 
+    @ApiOperation(value = "注册接口", notes = "注册接口")
     @PostMapping("/resgit")
-    public TokenVo Resgit(@RequestParam String userName, @RequestParam String passWrod, @RequestParam String resgitToken) throws Exception {
-        return usersService.resgit(userName, passWrod, resgitToken);
+    public TokenVo Resgit(@RequestParam String userName, @RequestParam String phone, @RequestParam String passWord, @RequestParam String phonecode) {
+        return usersService.resgit(userName, phone, passWord, phonecode);
+    }
+
+    @ApiOperation(value = "获取手机验证码", notes = "获取手机验证码")
+    @PostMapping("/getPhoneCode")
+    public ResultVo getPhoneCode(@RequestParam String phone, @RequestParam String resgitToken) throws Exception {
+        return usersService.getPhoneCode(phone, resgitToken);
+    }
+
+    @ApiOperation(value = "用户名是否注册", notes = "用户名是否注册")
+    @GetMapping("/CheckUserByName/{Name}")
+    public ResultVo CheckUserByName(@PathVariable String Name) {
+
+        if (usersService.CheckUserByName(Name)) {
+
+            return new ResultVo("恭喜您用户名可用", StatusVo.success, null);
+
+        } else {
+            return new ResultVo("用户名已被注册", StatusVo.Error, null);
+
+        }
+
+    }
+
+    @ApiOperation(value = "电话是否注册", notes = "电话是否注册")
+    @GetMapping("/CheckUserByPhone/{phone}")
+    public ResultVo CheckUserByPhone(@PathVariable String phone) {
+        if (usersService.CheckUserByPhone(phone)) {
+            return new ResultVo("手机号码未被注册", StatusVo.success, null);
+        } else {
+            return new ResultVo("手机号码已被注册,请您先登录", StatusVo.Error, null);
+
+        }
     }
 
 
--- service/src/main/java/com/PineappleStore/serviceImpl/UsersServiceImpl.java
+++ service/src/main/java/com/PineappleStore/serviceImpl/UsersServiceImpl.java
@@ -1,12 +1,15 @@
 package com.PineappleStore.serviceImpl;
 
+import com.PineappleStore.ResultVo.ResultVo;
 import com.PineappleStore.ResultVo.StatusVo;
 import com.PineappleStore.ResultVo.TokenVo;
+import com.PineappleStore.Utils.Client;
 import com.PineappleStore.Utils.DingxiangCheck;
 import com.PineappleStore.Utils.Md5Utils;
 import com.PineappleStore.dao.UsersMapper;
 import com.PineappleStore.entity.Users;
 import com.PineappleStore.service.UsersService;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import io.jsonwebtoken.JwtBuilder;
@@ -17,9 +20,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -38,7 +39,7 @@ public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements
     @Autowired
     private UsersMapper usersMapper;
 
-    public boolean CheckUserByname(String userName) {
+    public boolean CheckUserByName(String userName) {
 
         QueryWrapper<Users> checkwrapper = new QueryWrapper<>();
         checkwrapper.lambda().select(Users::getUsername).eq(Users::getUsername, userName).last("limit 1");
@@ -46,6 +47,16 @@ public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements
 
         return checkuser == null;
 
+    }
+
+    @Override
+    public boolean CheckUserByPhone(String Phone) {
+
+        QueryWrapper<Users> checkwrapper = new QueryWrapper<>();
+        checkwrapper.lambda().select(Users::getUserMobile).eq(Users::getUserMobile, Phone);
+        Users checkuser = usersMapper.selectOne(checkwrapper);
+
+        return checkuser == null;
     }
 
 
@@ -99,32 +110,89 @@ public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements
 
     @Override
     @Transactional
-    public TokenVo resgit(String userName, String passWord, String resgitToken) throws Exception {
+    public TokenVo resgit(String userName, String phone, String passWord, String phoneCode) {
 
         //线程锁
         synchronized (this) {
-            if (DingxiangCheck.Checktoken(resgitToken)) {
-                boolean checker = CheckUserByname(userName);
-                if (checker) {
-                    String md5pwd = Md5Utils.md5(passWord);//md5密码加密
-                    Users users = new Users();
-                    users.setUsername(userName);
-                    users.setPassword(md5pwd);
-                    users.setUserImg("img/user.jpg");
-                    users.setUserRegtime(new Date());
-                    users.setUserModtime(new Date());
-                    int i = usersMapper.insert(users);
-                    if (i > 0) {
-                        return new TokenVo("注册成功", 200, users, null);
-                    } else {
-                        return new TokenVo("注册失败", 404, null, null);
-                    }
+
+            boolean checker = CheckUserByName(userName);
+            if (checker) {
+                String md5pwd = Md5Utils.md5(passWord);//md5密码加密
+                Users users = new Users();
+                users.setUsername(userName);
+                users.setPassword(md5pwd);
+                users.setUserImg("img/user.jpg");
+                users.setUserRegtime(new Date());
+                users.setUserModtime(new Date());
+                int i = usersMapper.insert(users);
+                if (i > 0) {
+                    return new TokenVo("注册成功", 200, users, null);
                 } else {
-                    return new TokenVo("用户已经被注册了", 404, null, null);
+                    return new TokenVo("注册失败", 404, null, null);
                 }
             } else {
-                return new TokenVo("验证码过期", StatusVo.Error, null, null);
+                return new TokenVo("用户已经被注册了", 404, null, null);
             }
         }
+    }
+
+
+    /* 请求后端发送手机验证码*/
+    @Override
+    public ResultVo getPhoneCode(String phone, String resgitToken) throws Exception {
+
+        if (DingxiangCheck.Checktoken(resgitToken)) {
+
+
+            if (CheckUserByPhone(phone)) {
+
+                Client client = new Client();
+                client.setAppId("hw_10023");     //开发者ID，在【设置】-【开发设置】中获取
+                client.setSecretKey("0a35e267a5ab0e665b9a407031d94d71");    //开发者密钥，在【设置】-【开发设置】中获取
+                client.setVersion("1.0");
+
+                Client.Request request = new Client.Request();
+
+                //创建json 发送对象
+                JSONObject send = new JSONObject();
+
+                //随机生成4位随机验证码
+                Random randObj = new Random();
+                Integer phoneCode = (1000 + randObj.nextInt(9000));
+
+                //手机号
+                Set<String> phoneList = new HashSet<>();
+                phoneList.add(phone);
+                send.put("mobile", phoneList);
+
+                send.put("type", 0);
+                send.put("template_id", "ST_2020101100000005");
+                send.put("sign", "闪速码");
+                send.put("send_time", "");
+
+                //发送随机的验证码
+                Map<String, Integer> code = new HashMap<>();
+                code.put("code", phoneCode);
+                send.put("params", code);
+
+
+                request.setBizContent(String.valueOf(send));
+                request.setMethod("sms.message.send");
+
+
+                //发送验证码
+                //            client.execute(request);
+
+                return new ResultVo("验证码发送成功", StatusVo.success, client.execute(request));
+
+            } else {
+                return new ResultVo("手机号码已经被注册", StatusVo.Error, null);
+            }
+
+        } else {
+            return new ResultVo("人机校验过期", StatusVo.Error, null);
+        }
+
+
     }
 }

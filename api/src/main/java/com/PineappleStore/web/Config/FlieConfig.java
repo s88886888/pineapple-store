9e5af2c45bbc51cee03c904758e8f50255c8f308
Linson <854700937@qq.com>
2023-01-29 21:11:27 +0800

    修复token拦截失效

--- api/src/main/java/com/PineappleStore/web/Config/FlieConfig.java
+++ api/src/main/java/com/PineappleStore/web/Config/FlieConfig.java
@@ -1,7 +1,10 @@
 package com.PineappleStore.web.Config;
 
+import com.PineappleStore.web.Interceptor.TokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.system.ApplicationHome;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,5 +32,29 @@ public class FlieConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/imgs/**")
                 .addResourceLocations("file:" + pre);
+    }
+
+
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+//        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");//拦截所有
+
+//                .excludePathPatterns("/users/**")//放行用户登录
+//                .excludePathPatterns("/index-img/**")//放行轮播图
+//                .excludePathPatterns("/category/**")//商品分类
+//                .excludePathPatterns("/product/**")//商品信息
+//                .excludePathPatterns("/productimg/**")//商品图片
+
+//                .excludePathPatterns("/swagger-ui/**")//放行swagger
+//                .excludePathPatterns("/doc.html/**")//放行doc
+//                .excludePathPatterns("/swagger-resources/**")
+//                .excludePathPatterns("/v2/api-docs")
+//                .excludePathPatterns("/error")
+//                .excludePathPatterns("/webjars/**");
+
     }
 }

f09645063dab49b05ebde0b14de41530d430d343
Linson <854700937@qq.com>
2023-03-12 14:15:41 +0800

    添加图片上传，图片路径映射

--- api/src/main/java/com/PineappleStore/web/Config/FlieConfig.java
+++ api/src/main/java/com/PineappleStore/web/Config/FlieConfig.java
@@ -1,8 +1,9 @@
 package com.PineappleStore.web.Config;
 
 import com.PineappleStore.web.Interceptor.TokenInterceptor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.system.ApplicationHome;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -16,22 +17,21 @@ public class FlieConfig implements WebMvcConfigurer {
      * 图片保存路径，自动从yml文件中获取数据
      * 示例： E:/images/
      */
+    @Value("${filePath}")
+    private String filePath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        /**
-         * 配置资源映射
-         * 意思是：如果访问的资源路径是以“/images/”开头的，
-         * 就给我映射到本机的“E:/images/”这个文件夹内，去找你要的资源
-         * 注意：E:/images/ 后面的 “/”一定要带上
+        /*
+          配置资源映射
+          意思是：如果访问的资源路径是以“/images/”开头的，
+          就给我映射到本机的“E:/images/”这个文件夹内，去找你要的资源
+          注意：E:/images/ 后面的 “/”一定要带上
          */
 
-        ApplicationHome applicationHome = new ApplicationHome(this.getClass());
-        String pre = applicationHome.getDir().getParentFile().getParentFile()
-                .getAbsolutePath() + "\\src\\main\\resources\\imgs\\";
 
-        registry.addResourceHandler("/imgs/**")
-                .addResourceLocations("file:" + pre);
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:///" + filePath);
     }
 
 
@@ -39,7 +39,7 @@ public class FlieConfig implements WebMvcConfigurer {
     private TokenInterceptor tokenInterceptor;
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(@NotNull InterceptorRegistry registry) {
 
 //        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");//拦截所有
 

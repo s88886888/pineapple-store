99bf02e7450e4a045e62e47d11577101fda32e71
Linson <854700937@qq.com>
2023-05-15 02:59:05 +0800

    修正真实地址获取

--- common/src/main/java/com/PineappleStore/Utils/IPUtils.java
+++ common/src/main/java/com/PineappleStore/Utils/IPUtils.java
@@ -2,11 +2,8 @@ package com.PineappleStore.Utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 
 public class IPUtils {
@@ -23,52 +20,24 @@ public class IPUtils {
      * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
      */
     public static String getIpAddr(HttpServletRequest request) {
-        String ip = null;
-        try {
-            //以下两个获取在k8s中，将真实的客户端IP，放到了x-Original-Forwarded-For。而将WAF的回源地址放到了 x-Forwarded-For了。
-            ip = request.getHeader("X-Original-Forwarded-For");
-            if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getHeader("X-Forwarded-For");
-            }
-            //获取nginx等代理的ip
-            if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getHeader("x-forwarded-for");
-            }
-            if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getHeader("Proxy-Client-IP");
-            }
-            if (StringUtils.isEmpty(ip) || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getHeader("WL-Proxy-Client-IP");
-            }
-            if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_CLIENT_IP");
-            }
-            if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-            }
-            //兼容k8s集群获取ip
-            if (StringUtils.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getRemoteAddr();
-                if (LOCALHOST_IP1.equalsIgnoreCase(ip) || LOCALHOST_IP.equalsIgnoreCase(ip)) {
-                    //根据网卡取本机配置的IP
-                    InetAddress iNet = null;
-                    try {
-                        iNet = InetAddress.getLocalHost();
-                    } catch (UnknownHostException e) {
-                        logger.error("getClientIp error: {}", e);
-                    }
-                    ip = iNet.getHostAddress();
-                }
-            }
-        } catch (Exception e) {
-            logger.error("IPUtils ERROR ", e);
+        //目前则是网关ip
+        String ip = request.getHeader("X-Real-IP");
+        if (ip != null && !"".equals(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
         }
-        //使用代理，则获取第一个IP地址
-        if (!StringUtils.isEmpty(ip) && ip.indexOf(IP_UTILS_FLAG) > 0) {
-            ip = ip.substring(0, ip.indexOf(IP_UTILS_FLAG));
+        ip = request.getHeader("X-Forwarded-For");
+        if (ip != null && !"".equals(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            int index = ip.indexOf(',');
+            if (index != -1) {
+                //只获取第一个值
+                return ip.substring(0, index);
+            } else {
+                return ip;
+            }
+        } else {
+            //取不到真实ip则返回空，返回内网地址
+            return request.getRemoteAddr();
         }
-
-        return ip;
     }
 
 }
